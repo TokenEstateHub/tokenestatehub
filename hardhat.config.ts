d2a67fe9f186e90ef73cff7710c0bf7b91fa7d7b
Cavin <114779273+Killercavin@users.noreply.github.com>
2025-03-11 11:55:04 +0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,54 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+console.log(process.env.ALCHEMY_TESTNET_RPC_URL);
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_TESTNET_RPC_URL || "", // Ensure it's defined
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    lisksepolia: {
+      url: "https://rpc.sepolia-api.lisk.com",
+      chainId: 4202,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: "4RG4F3IGAC7ZXNUN9JQFEXAXBEYFJGRNXJ",
+      lisksepolia: "no-api-key-needed",
+    },
+    customChains: [
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://api-sepolia.etherscan.io/api",
+          browserURL: "https://sepolia.etherscan.io",
+        },
+      },
+      {
+        network: "lisksepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify.dev/server",
+    browserUrl: "https://repo.sourcify.dev",
+  },
 };
 
 export default config;
